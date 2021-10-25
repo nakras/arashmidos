4226cdf1735b3753b3caebae3adcc026aa981fe1
nakras <75827070+nakras@users.noreply.github.com>
2021-10-25 13:47:58 -0700

    Delete assignment 1.py

--- assignment 1.py
+++
@@ -1,7 +0,0 @@
-import random
-
-questions_list = ["who is your favorite actor/actress?", "what is your favorite color?", "what is your favorite food?"]
-answers_list = ["hmm... awesome!", "good choice", "okay cool "]
-for i in range(3):
-    input(questions_list[i])
-    print(random.choice(answers_list))
